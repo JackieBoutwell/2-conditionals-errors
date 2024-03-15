fc93151bd8a622661f2fb57f78031fbf14f3b2cf
Jackie Boutwell <boutwelljackie983@gmail.com>
2024-03-15 11:19:00 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// console.log(test3); 
+
+console.log('test');
+
+// alert('test')
+
+// var test3;
+
+
+
+// {
+//     let test = 123;
+//     const test2 = 123;
+//     // var test3 = 123;
+// }
+
+
+
+// console.log(test)
+
+var x = 10;
+let y = 5;
+
+{
+    let y = 6;
+	let z = x + y;
+    console.log(z);
+}
+
+var x123 = 100;
+
+console.log(x);
+console.log(y);
+
+
+let number = -2;
+
+if (false) {
+    // -> execute code here (if true)
+    console.log('x is 10');
+    console.log('x is 10');
+    console.log('x is 10');
+} else {
+    // else block
+    console.log('else')
+}
+console.log('next thing')
+
+
+let x = 5;
+
+if (x > 0) {
+	console.log('Positive!');
+    console.log('Positive!');
+    let test;
+} else {
+	console.log('Negative.');
+}
+
+// Exercise 1 -Else
+var num = 10
+
+if (num >= 0) {
+    console.log('Positive')
+} else {
+    console.log('Negative')
+}
+
+// Exercise 2- Else if
+var score = 88;
+
+if (score >= 90){
+    console.log('Grade A')
+}else if (score >= 80){
+    console.log('Grade B')
+}else if (score >= 60){
+    console.log('Grade C')
+}else if (score >= 50){
+    console.log('Grade D')
+}else{
+    console.log('Grade F')
+}
+
+    
